af559c78f81500b2930e4aaa4be8abaf20e07d00
Kenny Lindblom <kennylindblom93@gmail.com>
2023-10-25 11:10:09 +0200

    Admin can see all Users in the system

---
+++ LeaveManagementT5/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using LeaveManagementT5.Models;
+using LeaveManagementT5.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LeaveManagementT5.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+       
+
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+            
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult DisplayUsers()
+        {
+           
+           var users = _userManager.Users
+           .Select(u => new UserViewModel
+           {
+                
+             Id = u.Id,
+             UserName = u.UserName,
+             //Email = u.Email, //Email är exakt samma som username, känns inte nödvändgit att ha med det två gånger
+             PhoneNumber = u.PhoneNumber,
+             FirstName = (string)u.GetType().GetProperty("FirstName").GetValue(u, null),
+             LastName = (string)u.GetType().GetProperty("LastName").GetValue(u, null),
+                
+           }).ToList();
+
+           return View("DisplayUser", users);
+            
+        }
+
+
+
+    }
+}
---
+++ LeaveManagementT5/Models/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LeaveManagementT5.Models
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
